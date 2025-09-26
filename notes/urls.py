ebe342c91a3ba8aaeaa3bbc976f241b2cd182a85
Chandresh-spec <chandumoger819@gmail.com>
2025-09-27 00:32:26 +0530

    basic layout is done

--- notes/urls.py
+++ notes/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
    
     path('',views.check,name="check"),
     path('display/',views.display_items,name='display'),
-    path('add_note/',views.Add_view,name='add_note')
+    path('add_note/',views.Add_view,name='add_note'),
+    path('view/<int:pk>/',views.view_page,name='view_page'),
+    path('edit/<int:pk>/',views.edit_view,name='edit_page'),
+    path('delete/<int:pk>/',views.delete_view,name='delete_page')
     
+
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Note
 from .forms import AddNote
 # Create your views here.
@@ -28,6 +28,47 @@ def Add_view(request):
     else:
         form=AddNote()
         return render(request,'add_note.html',{'form':form})
+    
+
+
+def view_page(request,pk):
+    view=get_object_or_404(Note,pk=pk)
+    return render(request,'view.html',{'view':view})
+
+
+
+def edit_view(request,pk):
+    st=get_object_or_404(Note,pk=pk)
+    if request.method=='POST':
+        form=AddNote(request.POST,instance=st)
+
+        if form.is_valid():
+            form.save()
+            return redirect('display')
+        
+        return render(request,'edit.html',{'form':form})
+
+    form=AddNote(instance=st)
+    return render(request,'edit.html',{'form':form})
+
+
+
+def delete_view(request,pk):
+    note=get_object_or_404(Note,pk=pk)
+
+    if request. method=='POST':
+        note.delete()
+        return redirect('display')
+    
+
+    return render(request,'delete.html',{'note':note})
+
+
+
+
+
+
+
 
 
 
